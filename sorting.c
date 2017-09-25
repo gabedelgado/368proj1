da9ed9ec7714fb02b765417c839890187b72f440
gabedelgado <delgadog@purdue.edu>
2017-09-25 12:58:37 -0400

    first commit

---
+++ sorting.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sorting.h>
+
+
+//long * Load_File(char * Filename, int * size)
+{
+	FILE * dafile; 
+	dafile = fopen(Filename, "r");
+
+	if (dafile == NULL)
+	{
+		printf("File not could not be opened! \n");
+	}
+	
+
+	fscanf(dafile, "%d", size);
+	long * array[*size];
+
+	for (int i = 0; i < *size; i++)
+	{
+		fscanf(dafile, "%li", array[i]);
+	}
+	
+	fclose(Filename);
+	
+	return * array;
+}
