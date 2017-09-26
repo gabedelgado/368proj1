eecd71441dff464c1fea37dc272b228a8ffd28f5
gabedelgado <delgadog@purdue.edu>
2017-09-25 21:42:04 -0400

    added genseq1 function

--- sorting.c
+++ sorting.c
@@ -29,8 +29,43 @@ long * Load_File(char * Filename, int * size)
 
 void genseq1(int * array, int N)
 {
+	array = (int *)calloc(N, sizeof(int));
 	
+	int p2 = 0;
+	int p3 = 0;
+	array[0] = 1;	
+	int lead = 1;
+	int u2 = 1;
+	int u3 = 1;
 
-	
+	while(array[lead-1] < N)
+	{
+		if (array[p2] * 2 == array[lead-1])
+		{
+			p2 = p2 + 1;
+		}
+		if (array[p3] * 3 == array[lead-1])
+		{
+			p3 = p3 + 1;
+		}
+		u2 = array[p2] * 2;
+		u3 = array[p3] * 3;
+		
+		if (u2 < u3)
+		{
+			p2 = p2 + 1;
+			array[lead] = u2;
+		}
+		else
+		{
+			p3 = p3 + 1;
+			array[lead] = u3;
+		}
+		lead++;
+	}	
+	array[lead-1] = 0;
+
 }
+
+
 
